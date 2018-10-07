ad489cb2ca9c3510fbf4e257e8e6094e2d5a545c
DavidMChan <david@iridescent.io>
2018-10-07 11:50:54 -0700

    Fixed sutupid bug with Dense stacking

--- rinokeras/models/transformer.py
+++ rinokeras/models/transformer.py
@@ -338,13 +338,15 @@ class TransformerInputEmbedding(tf.keras.Model):
             assert n_embed_layers == 1, 'discrete models can only have one embedding layer'
             if embedding_initializer is not None:
                 assert (embedding_initializer.shape[0] == n_symbols) in [None, True], 'n_symbols and initializer shape mismatch'
-                self.embedding = tf.keras.layers.Embedding(n_symbols, embed_size,
+                self.embedding_t = tf.keras.layers.Embedding(n_symbols, embed_size,
                                                            weights=[embedding_initializer],
                                                            mask_zero=True)
                 
                 if shape[1] == embed_size in [None, True]:
                     # We have to correct if the input embedding isn't quite right
-                    self.embedding = tf.keras.layers.Dense(embed_size)
+                    self.embedding = Stack([self.embedding_t, tf.keras.layers.Dense(embed_size)])
+                else:
+                    self.embedding = self.embedding_t
             else:
                 self.embedding = tf.keras.layers.Embedding(
                     n_symbols, embed_size)
