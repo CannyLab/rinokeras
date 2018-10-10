739ed4ff164f7083ec11981bf97b9996063bd06a
Roshan Rao <roshan_rao@berkeley.edu>
2018-10-10 14:57:53 -0700

    fix __init__.py

--- rinokeras/__init__.py
+++ rinokeras/__init__.py
@@ -1,4 +1,4 @@
-import utils
-from run_context import train_epoch, test_epoch
+import rinokeras.utils
+from rinokeras.run_context import train_epoch, test_epoch
 
 __all__ = ['common', 'models', 'rl', 'test', 'trainers', 'run_context']
