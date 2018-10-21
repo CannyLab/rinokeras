ea7557fb449b299ebacc4e6e1c7a60a9f9fefc12
Roshan Rao <roshan_rao@berkeley.edu>
2018-10-21 15:39:27 -0700

    auto-import trainers module

--- rinokeras/__init__.py
+++ rinokeras/__init__.py
@@ -1,5 +1,7 @@
 import rinokeras.utils  # noqa: F401
 import rinokeras.common as layers
+import rinokeras.models
+import rinokeras.trainers
 from rinokeras.run_context import train_epoch, test_epoch  # noqa: F401
 
 __all__ = ['common', 'models', 'rl', 'test', 'trainers', 'utils', 'run_context', 'layers']
