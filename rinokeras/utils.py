1aaa1f204a660f3d863439947c4211a16b478c72
Roshan Rao <roshan_rao@berkeley.edu>
2018-12-06 17:54:38 -0800

    change load distributed

--- rinokeras/utils.py
+++ rinokeras/utils.py
@@ -200,9 +200,9 @@ def get_optimizer(optimizer, learning_rate=1e-3):
         raise ValueError("Unrecognized optimizer. Received {}.".format(optimizer))
 
 
-def load_distributed(distribution_strategy, model, filename):
+def load_distributed(distribution_strategy, model, filename, by_name=False):
     with distribution_strategy.scope():
-        model.load_weights(filename)
+        model.load_weights(filename, by_name=by_name)
         weights = model.get_weights()
         assign_ops = []
         for layer in model.layers:
