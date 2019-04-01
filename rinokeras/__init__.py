cdbc67c4a0c1437fa0ae0714d29c93345b4fe280
DavidMChan <david@iridescent.io>
2019-04-01 13:28:15 -0700

    Added expanded testing support
    Former-commit-id: ad455e80aca11dd3d92cf148046a0775866c4621

--- rinokeras/__init__.py
+++ rinokeras/__init__.py
@@ -2,11 +2,14 @@ import tensorflow as tf
 from packaging import version
 
 RK_USE_TF_VERSION = None
-if version.parse(tf.__version__) < version.parse("2.0.0"):
+if version.parse(tf.__version__) < version.parse("2.0.0a0"):
     RK_USE_TF_VERSION = 1
+    import rinokeras.compat
+    import rinokeras.layers
+    import rinokeras.models
 else:
     RK_USE_TF_VERSION = 2
+    import rinokeras.compat
+    import rinokeras.layers
+    import rinokeras.models
 
-import rinokeras.compat
-import rinokeras.layers
-import rinokeras.models
--- rinokeras/layers/__init__.py
+++ rinokeras/layers/__init__.py
@@ -58,4 +58,4 @@ if _RK_USE_TF_VERSION == 1:
     # GCN
     from rinokeras.core.v1x.common import GraphConvolutionalLayer
 elif _RK_USE_TF_VERSION == 2:
-    raise NotImplementedError('Layers not yet supported in RK2.0')
+    pass
