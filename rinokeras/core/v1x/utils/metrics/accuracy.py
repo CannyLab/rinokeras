0beba3c471cea893df418fb1242da1552863db42
Roshan Rao <roshan_rao@berkeley.edu>
2019-04-05 14:23:35 -0700

    fix bug in None check for weights
    Former-commit-id: 13b0f577e6bb0ac991c76345dc1e6f5d3520587d

--- rinokeras/core/v1x/utils/metrics/accuracy.py
+++ rinokeras/core/v1x/utils/metrics/accuracy.py
@@ -1,10 +1,9 @@
 import tensorflow as tf
 
-def accuracy(labels, predictions, weights=None, dtype=tf.float32):
+
+def accuracy(labels, predictions, weights=1.0, dtype=tf.float32):
     is_correct = tf.equal(labels, predictions)
     is_correct = tf.cast(is_correct, dtype)
-    if not weights:
-        return tf.reduce_mean(is_correct)
-    
+
     weights = tf.ones_like(is_correct) * weights
     return tf.reduce_sum(is_correct * weights) / tf.reduce_sum(weights)
