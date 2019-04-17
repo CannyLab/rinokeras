e30308d1c3a10b7b35659698b4936c61fb0e1387
Roshan Rao <roshan_rao@berkeley.edu>
2019-04-17 14:01:56 -0700

    fix bug in output size of Ray policiy

--- rinokeras/core/v1x/rl/RayPolicy.py
+++ rinokeras/core/v1x/rl/RayPolicy.py
@@ -16,7 +16,6 @@ def ray_policy(model: Type[tf.keras.Model]):
 
         @override(ray.rllib.models.Model)
         def _build_layers_v2(self, input_dict, num_outputs, options):
-            self.obs_in = input_dict["obs"]
             if tf.get_collection('model'):
                 self.model = tf.get_collection('model')[0]
             else:
@@ -24,25 +23,17 @@ def ray_policy(model: Type[tf.keras.Model]):
                 tf.add_to_collection('model', self.model)
 
             if self.model.recurrent:
-                dummy = tf.placeholder(tf.float32, [None])
-                initial_state = self.model.get_initial_state(dummy)
+                self.state_init = [
+                    np.zeros([state_size]) for state_size in self.model.state_size]
 
                 if not self.state_in:
-                    self.state_in = \
-                        [tf.placeholder(state.dtype, state.shape) for state in initial_state]
+                    self.state_in = [tf.placeholder(tf.float32, [None, state_size])
+                                     for state_size in self.model.state_size]
 
-                def get_numpy_state(state):
-                    shape = state.shape.as_list()
-                    if shape[0] is None:
-                        shape = shape[1:]
-                    assert not any(s is None for s in shape), 'Found None in initial state shape'
-                    dtype = state.dtype.as_numpy_dtype
-                    return np.zeros(shape, dtype)
-
-                self.state_init = [get_numpy_state(state) for state in initial_state]
-
-                output = self.model(input_dict, seqlens=self.seq_lens, initial_state=self.state_in)
-                self.state_out = output['state_out']
+                output = self.model(input_dict,
+                                    seqlens=self.seq_lens,
+                                    initial_state=self.state_in)
+                self.state_out = list(output['state_out'])
             else:
                 output = self.model(input_dict)
 
